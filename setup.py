cf1a3366d63473e538db09016fb5fb125033d634
Crimson206 <sisung.kim1@gmail.com>
2023-12-08 15:00:35 +0100

    added setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="DataLoader",
+    version="0.1.0",
+    description="Personal DataLoader Module",
+    author="Sisung Kim",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    install_requires=[
+        "numpy",
+        "torch",
+        "torchvision",
+    ],
+)
