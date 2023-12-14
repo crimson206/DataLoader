2af90fb0e85343a6a3c58593139b90d650e7872a
Crimson206 <sisung.kim1@gmail.com>
2023-12-14 15:30:01 +0100

    added cifar

--- src/CrimsonDataLoader/dataloads/dataloads.py
+++ src/CrimsonDataLoader/dataloads/dataloads.py
@@ -2,25 +2,41 @@ import torch
 import torchvision
 import torchvision.transforms as transforms
 
-def load_mnist(root="./data", batch_size=64):
+def load_mnist(root="./data", batch_size=64, download=False):
     transform = transforms.Compose([
         transforms.ToTensor(),
         transforms.Normalize((0.5,), (0.5,)),
     ])
 
     # Download and load the training data
-    trainset = torchvision.datasets.MNIST(root, download=True, train=True, transform=transform)
+    trainset = torchvision.datasets.MNIST(root, download=download, train=True, transform=transform)
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True)
 
-    testset = torchvision.datasets.MNIST(root, download=True, train=False, transform=transform)
+    testset = torchvision.datasets.MNIST(root, download=download, train=False, transform=transform)
     testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False)
     
     return trainset, trainloader, testset, testloader
 
+def load_cifar(root="./data", batch_size=64, download=False, num_classes=10):
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),  # CIFAR images are in color
+    ])
 
-transform = transforms.Compose([
-    transforms.ToTensor(),
-    # Add other transformations here if needed
-    # Example: transforms.Resize((224, 224)),
-    # Example: transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-])
+    # Choose the dataset based on the number of classes
+    if num_classes == 10:
+        dataset = torchvision.datasets.CIFAR10
+    elif num_classes == 100:
+        dataset = torchvision.datasets.CIFAR100
+    else:
+        raise ValueError("num_classes must be 10 or 100")
+
+    # Download and load the training data
+    trainset = dataset(root, download=download, train=True, transform=transform)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True)
+
+    # Download and load the test data
+    testset = dataset(root, download=download, train=False, transform=transform)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False)
+    
+    return trainset, trainloader, testset, testloader
