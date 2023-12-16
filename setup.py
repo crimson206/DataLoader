4ca6525f9a55a9ebbbb633b399f7cd9cd2b6ba48
Crimson206 <sisung.kim1@gmail.com>
2023-12-16 23:49:20 +0100

    updated version 0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="DataLoader",
-    version="0.1.1",
+    version="0.1.2",
     description="Personal DataLoader Module",
     author="Sisung Kim",
     package_dir={"": "src"},
