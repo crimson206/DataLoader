a0e36d94cc04ed7b6be1ef7743f97c2828fa3c62
Crimson206 <sisung.kim1@gmail.com>
2023-12-16 23:46:58 +0100

    updated mnist fashion option

--- src/CrimsonDataLoader/dataloads/dataloads.py
+++ src/CrimsonDataLoader/dataloads/dataloads.py
@@ -2,8 +2,33 @@ import torch
 import torchvision
 import torchvision.transforms as transforms
 
-def load_mnist(root="./data", batch_size=64, download=False):
+def load_mnist(root="./data", batch_size=64, download=False, resize_to=32, dataset_type='mnist'):
     transform = transforms.Compose([
+        transforms.Resize((resize_to, resize_to)),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5,), (0.5,)),
+    ])
+
+    if dataset_type == 'mnist':
+        dataset = torchvision.datasets.MNIST
+    elif dataset_type == 'fashion':
+        dataset = torchvision.datasets.FashionMNIST
+    else:
+        raise ValueError("Invalid dataset type. Choose 'mnist' or 'fashion'.")
+
+    # Download and load the training data
+    trainset = dataset(root, download=download, train=True, transform=transform)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True)
+
+    testset = dataset(root, download=download, train=False, transform=transform)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False)
+    
+    return trainset, trainloader, testset, testloader
+
+
+def load_mnist(root="./data", batch_size=64, download=False, resize_to=32):
+    transform = transforms.Compose([
+        transforms.Resize((resize_to, resize_to)),
         transforms.ToTensor(),
         transforms.Normalize((0.5,), (0.5,)),
     ])
